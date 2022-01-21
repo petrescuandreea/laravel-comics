0fd6564f1854a820a8a07386c003f8b85738b569
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-21 15:34:46 +0100

    Add footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,106 @@
+<footer>
+    <section id="footer-nav">
+        <div class="container">
+            <div id="nav-container">
+                <div id="left-nav">
+                    <h3>Dc Comics</h3>
+
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Tv</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <h3>Shop</h3>
+
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+
+                <div id="mid-nav">
+                    <h3>Dc</h3>
+
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div id="right-nav">
+                    <h3>Sites</h3>
+
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="img">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="DC logo">
+            </div>
+        </div>
+
+    </section>
+
+    <section id="footer-sm-channels">
+        <div class="container">
+            <button>
+                Sign-up now!
+            </button>
+    
+            <div id="sm-channels">
+                <span>Follow us</span>
+                <ul id="social-icons">
+                    <li>
+                        <a href="#">
+                            <img src="" alt="">1
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="" alt="">2
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="" alt="">3
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="" alt="">4
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="" alt="">5
+                        </a>
+                    </li>
+                </ul>
+    
+            </div>
+        </div>
+
+    </section>
+
+</footer>
