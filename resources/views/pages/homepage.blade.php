fcfc51db0da4c1498ee5722257fea819679e369b
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-21 21:59:48 +0100

    Add shop section to homepage component

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -135,4 +135,43 @@
             </div>
         </div>
     </section>
+
+    <section id="home-page-shop">
+        <ul class="container">
+            <li>
+                <a href="#">
+                    <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="">
+                    <span>Digital comics</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="">
+                    <span>Dc merchandise</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="">
+                    <span>Subscription</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="">
+                    <span>Comic shop locator</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="">
+                    <span>Dc power visa</span>
+                </a>
+            </li>
+        </ul>
+    </section>
 @endsection
