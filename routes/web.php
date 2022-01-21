4547295cd0484c9f8014de3ae701355780f87716
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-21 17:43:52 +0100

    Create single-page route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.homepage');
 }) -> name('homepage');
+
+Route::get('/comic', function () {
+    return view('pages.single-page');
+}) -> name('single-page');
