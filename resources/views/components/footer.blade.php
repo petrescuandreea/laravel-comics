fdc8a3b7bdec3251a1583bd05f3e13f1e93618a4
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-23 20:51:46 +0100

    Add content to footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,61 +2,69 @@
     <section id="footer-nav">
         <div class="container">
             <div id="nav-container">
-                <div id="left-nav">
-                    <h3>Dc Comics</h3>
-
-                    <ul>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Tv</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">News</a></li>
-                    </ul>
-
-                    <h3>Shop</h3>
-
-                    <ul>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
-                    </ul>
+                <div id="nav-links">
+                    <div id="left-nav">
+                        <h3>Dc Comics</h3>
+    
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Tv</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+    
+                        <h3>Shop</h3>
+    
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+    
+                    <div id="mid-nav">
+                        <h3>Dc</h3>
+    
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+    
+                    <div id="right-nav">
+                        <h3>Sites</h3>
+    
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div id="mid-nav">
-                    <h3>Dc</h3>
-
-                    <ul>
-                        <li><a href="#">Terms Of Use</a></li>
-                        <li><a href="#">Privacy policy (New)</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Advertising</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">Talent Workshops</a></li>
-                        <li><a href="#">CPSC Certificates</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </div>
-
-                <div id="right-nav">
-                    <h3>Sites</h3>
-
-                    <ul>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
-                    </ul>
+                <div id="copyright">
+                    <p> 
+                        Site Content TM and &#169; 2020 DC Entertainment, unless otherwise <a href="#"> noted here </a>. All rights reserved. <a href="#"> Cookies Settings </a>
+                    </p>
                 </div>
 
             </div>
 
-            <div class="img">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="DC logo">
+            <div class="logo">
+                <img src="{{ asset("img/dc-logo-bg.png") }}" alt="DC logo">
             </div>
         </div>
 
@@ -73,34 +81,31 @@
                 <ul id="social-icons">
                     <li>
                         <a href="#">
-                            <img src="" alt="">1
+                            <img src="{{ asset("img/footer-facebook.png") }}" alt="fb-logo">
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            <img src="" alt="">2
+                            <img src="{{ asset("img/footer-twitter.png") }}" alt="twitter logo">
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            <img src="" alt="">3
+                            <img src="{{ asset("img/footer-youtube.png") }}" alt="youtube logo">
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            <img src="" alt="">4
+                            <img src="{{ asset("img/footer-pinterest.png") }}" alt="pinterest logo">
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            <img src="" alt="">5
+                            <img src="{{ asset("img/footer-periscope.png") }}" alt="periscope logo">
                         </a>
                     </li>
                 </ul>
-    
-            </div>
+            </div> 
         </div>
-
     </section>
-
 </footer>
