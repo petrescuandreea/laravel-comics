7013ef58151bcd77059c9125ce2112949f163984
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-24 16:55:41 +0100

    Create link in single-page to homepage

--- resources/views/pages/single-page.blade.php
+++ resources/views/pages/single-page.blade.php
@@ -4,7 +4,7 @@
         <div class="bg-light-blue">
             <div class="container-center">
                 <div class="comic-img-wrapper">
-                    <a href="#">
+                    <a href="{{ route('homepage') }}">
                         <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="comic image">
                         <div class="gallery">
                             <span>View gallery</span>
