6ef990d55efab56fe3fcaa9b42bcc69a5f122746
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-24 17:44:17 +0100

    Use config function in homepage route

--- routes/web.php
+++ routes/web.php
@@ -4,58 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-
-    $comicSeries = [
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
-          "series" => "Action Comics"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
-          "series" => "American Vampire 1976"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
-          "series" => "Aquaman"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
-          "series" => "Batgirl"
-        ],
-        [
-          "image" =>"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
-          "series" => "Batman"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
-          "series" =>"Batman Beyond"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
-          "series" => "Batman/Superman"
-        ],
-        [
-          "image" =>"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
-          "series" => "Batman/Superman Annual"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
-          "series" => "Batman: The Joker War Zone"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
-          "series" => "Batman: Three Jokers"
-        ],
-        [
-          "image" =>"https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
-          "series" =>"Batman: White Knight Presents: Harley Quinn"
-        ],
-        [
-          "image" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
-          "series" => "Catwoman"
-        ]
-    ];
-
+    $comicSeries = config('data.comicSeries');
     return view('pages.homepage', compact('comicSeries'));
 }) -> name('homepage');
 
