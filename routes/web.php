bf1217cd27a62fed3458f826da5bf6e53c16aaf7
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-25 17:18:36 +0100

    Use HomePage controller in route homepage

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    $comicSeries = config('data.comicSeries');
-    return view('pages.homepage', compact('comicSeries'));
-}) -> name('homepage');
+// config function 
+// Route::get('/', function () {
+//     $comicSeries = config('data.comicSeries');
+//     return view('pages.homepage', compact('comicSeries'));
+// }) -> name('homepage');
+
+Route::get('/', 'HomePageController@homePage') -> name('homepage');
+    
 
 Route::get('/comic', function () {
     return view('pages.single-page');
